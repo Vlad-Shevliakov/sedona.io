d31863e85f368ca1641ae6a7be71f0bd44b54407
Vlad-Shevliakov <vladislavs319@gmail.com>
2018-11-08 12:46:25 +0200

    👌

--- main.js
+++ main.js
@@ -4,17 +4,13 @@ let hiddenItem = document.querySelectorAll(".site-navigation__item");
 let hiddenItemLength = hiddenItem.length;
     btnShow.addEventListener("click", function() {
         btnShow.classList.add("js-hide-btn");
-        for (let k = 0; k <= 3; k++) {
-            hiddenItem[k].classList.remove("hide-elem");
+        for (let item of hiddenItem) {
+            item.classList.remove("hide-elem");
         }
     });
     btnHide.addEventListener("click", function() {
         btnShow.classList.remove("js-hide-btn");
-        for (let k = 0; k <= 3; k++) {
-            hiddenItem[k].classList.add("hide-elem");
+        for (let item of hiddenItem) {
+            item.classList.add("hide-elem");
         }
     });
-
-//for (let k = 0; k <= hideElem.length; k++) - такой вариант вызвал Uncaught TypeError: Cannot read property 'classList' of undefined
-                                                                    // at HTMLButtonElement.<anonymous>
-
